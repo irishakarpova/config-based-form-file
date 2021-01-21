8af2883a7ca1152179b3bdfa889231f8ec11fa85
irina <irisha.karpova@gmail.com>
2021-01-21 13:15:06 -0600

    update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import { ThemeProvider } from '@material-ui/core/styles';
 import { CssBaseline } from '@material-ui/core';
 import Congfigform from './components/marketing/manage-banners'
+import theme from './theme'
 
 
 function App() {
