ff4ec44c58406cbd4b197f8941b6bde992821130
irina <irisha.karpova@gmail.com>
2021-01-21 16:21:50 -0600

    update

--- src/components/marketing/manage-banners.tsx
+++ src/components/marketing/manage-banners.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
-import Grid from '@material-ui/core/Grid';
 import CreateForm from '../forms/createForm'
 import SnackbarInt from '../../utility/snackBar'
 
@@ -9,17 +8,16 @@ import SnackbarInt from '../../utility/snackBar'
 const Configform: React.FC  = (): JSX.Element => {
 
   return(
-        <Grid item xs={12} sm={10} md={6} lg={5} xl={4}>
+        <React.Fragment>
           <Box p={3}>
             <SnackbarInt 
               text='Banner successfully created!'/>
-            
             <Typography variant='h3' color="primary">
               Create a new Banner
             </Typography>
             <CreateForm/>
           </Box>
-        </Grid>
+        </React.Fragment>
 
 
   ) 
